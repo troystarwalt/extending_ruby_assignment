1d4863b9ba07d51d5a4c45f3d731820b51cdf89a
Troy Starwalt <troy.starwalt@gmail.com>
2015-10-03 19:23:38 -0400

    initial commit

---
+++ basicSinatraWebApp/app.rb
@@ -0,0 +1,46 @@
+require "sinatra"
+require "mandrill"
+require "geolocater"
+require "tilt/erubis"
+
+
+m = Mandrill::API.new
+
+set :bind, "0.0.0.0"
+
+get "/home" do
+	erb :index
+end
+
+get "/about" do
+	erb :about
+end
+
+get "/array" do
+	erb :array
+end
+
+post "/sendmail" do
+	if !params[:u_email].nil? &&
+	   !params[:flname].nil? &&
+	   !params[:email_body].nil?
+
+		message = {
+				:subject=> "Hello from the sky!",
+				:from_name=> "Test",
+				:text=>"Word up?",
+				:to=>[{:email=> "#{params[:u_email]}",
+						:name=> "#{params[:fl_name]}"}
+					],
+				:html=>"<html>#{params[:email_body]}
+						<p><i>#{params[:flname]} sent you this message. You should thank them!
+						</p></i>
+						</html>",
+				:from_email=>"#{params[:f_email]}"
+			}
+		sending = m.messages.send(message)
+		"#{sending}"
+	else
+		"You forgot to fill out all of the fields!"
+	end
+end
